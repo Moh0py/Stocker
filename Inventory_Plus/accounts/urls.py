c989cd40cdefdd97e1c0713b4ac39c22f4eb7061
Moh0py <1vipmoh@gmail.com>
2025-08-12 10:27:11 +0300

    fixed some issues

--- Inventory_Plus/accounts/urls.py
+++ Inventory_Plus/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import CustomLoginView, SignUpView, CustomLogoutView
+from .views import CustomLoginView, SignUpView, CustomLogoutView 
 
 app_name = 'accounts'
 
